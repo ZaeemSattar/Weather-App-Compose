3db913acda287d913db199c5a46d2fce413abe9e
Zaeem Sattar <zaeem.sattar@synavos.com>
2022-06-07 01:10:44 +0500

    splash screen completed with navigation

---
+++ app/src/main/java/com/zaeem/weatherappcompose/ui/screens/SplashScreen.kt
@@ -0,0 +1,75 @@
+package com.zaeem.weatherappcompose.ui.screens
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.zaeem.weatherappcompose.R
+import com.zaeem.weatherappcompose.ui.navigation.Screen
+import com.zaeem.weatherappcompose.ui.theme.WeatherAppComposeTheme
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 2000,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        delay(3000L)
+
+        navController.navigate(Screen.Home.route) {
+            popUpTo(Screen.Splash.route) {
+                inclusive = true
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.cloudy),
+            contentDescription = "splash icon",
+            modifier = Modifier.scale(scale = scale.value)
+        )
+
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherAppComposeTheme {
+        SplashScreen(navController = rememberNavController())
+    }
+}
